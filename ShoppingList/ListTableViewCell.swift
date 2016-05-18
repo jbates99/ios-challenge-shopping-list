27304271d3b7bbf0da316e28f83f74023f69e112
Joshua Bates <batesjoshua50@gmail.com>
2016-05-17 19:23:06 -0600

    set up outlets and actions for cell

--- ShoppingList/ListTableViewCell.swift
+++ ShoppingList/ListTableViewCell.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ListTableViewCell: UITableViewCell {
 
+    @IBOutlet var button: UIButton!
+    @IBOutlet var primaryLabel: UILabel!
+    
+    weak var delegate: ListTableViewCellDelegate?
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        if delegate != nil {
+            delegate?.ListCellButtonTapped(self)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +32,23 @@ class ListTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension ListTableViewCell {
+    func updateButton(isComplete: Bool) {
+        if isComplete {
+            button.setImage(UIImage(named: "complete"), forState: .Normal)
+        } else {
+            button.setImage(UIImage(named: "incomplete"), forState: .Normal)
+        }
+    }
+    
+    func updateWithItem(item: Item) {
+        primaryLabel.text = item.name
+        updateButton(item.isComplete!.boolValue)
+    }
+}
+
+protocol ListTableViewCellDelegate: class {
+    func ListCellButtonTapped(sender: ListTableViewCell)
 }
