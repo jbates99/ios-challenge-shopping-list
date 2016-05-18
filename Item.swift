a9f11dea6097728139528331afa1ae4075eec177
Joshua Bates <batesjoshua50@gmail.com>
2016-05-17 19:22:14 -0600

    added stack file

---
+++ Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Joshua Bates on 5/17/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Item: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ ItemController.swift
@@ -0,0 +1,9 @@
+//
+//  ItemController.swift
+//  ShoppingList
+//
+//  Created by Joshua Bates on 5/17/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
