68f402e655835d0415a41d5322ed3839a6108966
Joshua Bates <batesjoshua50@gmail.com>
2016-05-17 19:22:50 -0600

    set up itemController

--- ItemController.swift
+++ ItemController.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import CoreData
+
+class ItemController {
+    static let sharedController = ItemController()
+    private let itemKey = "items"
+    
+    var items: [Item] {
+        let request = NSFetchRequest(entityName: "Item")
+        let moc = Stack.sharedStack.managedObjectContext
+        let result = (try? moc.executeFetchRequest(request)) as? [Item]
+        return result ?? []
+    }
+    
+    func addItem(name: String, context: NSManagedObjectContext = Stack.sharedStack.managedObjectContext) {
+        let _ = Item(name: name, context: context)
+        saveToPersistentStorage()
+    }
+    
+    func removeItem(item: Item) {
+        if let moc = item.managedObjectContext {
+            moc.deleteObject(item)
+            saveToPersistentStorage()
+        }
+    }
+    
+ /*   func filePath(key: String) -> String {
+        let directorySearchResults = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true)
+        let documentsPath: AnyObject = directorySearchResults[0]
+        let entriesPath = documentsPath.stringByAppendingString("/\(key).plist")
+        
+        return entriesPath
+    }
+ */
+    
+    func saveToPersistentStorage() {
+        _ = try? Stack.sharedStack.managedObjectContext.save()
+    }
+}
